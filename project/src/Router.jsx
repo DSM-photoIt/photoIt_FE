0ecb289c751b4a7884fff6b959e37e921ceddc2c
박지연 <163213733+j02on@users.noreply.github.com>
2024-10-28 13:31:07 +0900

    FEAT :: Router 설정

--- project/src/Router.jsx
+++ project/src/Router.jsx
@@ -1,10 +1,12 @@
-import { MainPage } from './pages/MainPage';
+import { MainPage } from './pages';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { PhotoShootPage } from './pages';
 export const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="/photoshoot" element={<PhotoShootPage />} />
       </Routes>
     </BrowserRouter>
   );
